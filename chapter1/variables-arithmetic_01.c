434fa2aac03e861281122ef8db8bffcef3a8348c
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-13 20:01:02 -0300

    update

--- chapter1/variables-arithmetic_01.c
+++ chapter1/variables-arithmetic_01.c
@@ -22,3 +22,13 @@ main()
 	}
 }
 
+/*
+	%d    print as decimal integer
+	%6d   print as decimal integer, at least 6 charecters wide
+	%f    print as floating point
+	%6f   print as floating point, at least 6 characters wide
+	%.2f  print as floating point, 2 characters after decimal point
+	%6.2f print as floating point, at least 6 wide and 2 after decimal point
+
+*/
+
