dc5404fd32bbe5c5d4d90aa56f9535713a4bf4c9
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-13 19:44:52 -0300

    primeiro commit

---
+++ chapter1/getting-started.c
@@ -0,0 +1,41 @@
+/*in c, the program to print "hello, world!"*/ 
+
+#include <stdio.h>    //include information about standard library
+#include <conio.h>
+
+
+
+int main(){    //define a function named main that receives no argument values
+	
+	
+	printf("hello, world!\n");   //main malls library function printf to print this sequence of characters;
+ 				     // \n represents the newline character		             
+
+	
+
+
+	printf("hello, ");
+	printf("world!");
+	printf("\n");
+
+
+
+
+	return hello; 
+
+	/*
+	
+	\n = only a single character (invisible characters)
+	\t = tab
+	 \b = backspace
+
+
+
+	when out #include <stdio.h> =  incompatible implicit declaration of built-in function 'printf'
+	when out int main(){ =  expected declaration specifiers or '...' before string constant
+	*/
+
+
+}
+
+
