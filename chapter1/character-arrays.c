8d4974c47a4443857d15bedc5c8cfec89e1dfcfc
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-23 21:23:42 -0300

    update

---
+++ chapter1/character-arrays.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* maximum input line size*/
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/*print longest input line*/
+
+main()
+{
+	int len;   /*current line length*/
+	int max;	/*maximum lenght seen so far*/
+	char line[MAXLINE];   /*current input line*/
+	char longest[MAXLINE]; /*longest line saves here*/
+
+max = 0;
+
+	while((len = getline(line, MAXLINE)) > 0)
+	if (len > max){
+		max = len;
+		copy(longest, line);
+	}
+	if (max>0 ) /* there was a line*/
+		printf("%s", longest);
+	return 0;
+
+
+}
